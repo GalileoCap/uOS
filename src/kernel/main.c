a3dd9ccf6b63de2b1a8c1577444ceee9c376d7f0
GalileoCap <galileocapp@gmail.com>
2023-04-19 00:21:34 -0300

    OK: Serial I/O

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,9 +1,9 @@
-#include <mmu.h>
-#include <vfs.h>
-#include <idt.h>
-#include <ata.h>
+/*#include <mmu.h>*/
+/*#include <vfs.h>*/
+/*#include <idt.h>*/
+/*#include <ata.h>*/
 #include <serial.h>
-#include <i686.h>
+/*#include <i686.h>*/
 #include <utils.h>
 
 //U: Define global variables //NOTE: Their values get set somewhere else
@@ -17,27 +17,25 @@ void kmain(void *mbd, bool magicError) {
 
   if (serial_init(COM1) != EOK) panic("[kmain] Serial failed errno=%X\n", errno);
   printf("[kmain] Testing format: %d %i %u %z %x %X %p %s %c %% %a\n", 1, -1, -2, 1025 * 0x1000, 0x6007F337, 0x600DB007, kmain, "\"A string\"", 'G');
+
+  /*
   if (mmu_init(mbd) != EOK) panic("[kmain] MMU failed errno=%X\n", errno);
   if (idt_init() != EOK) panic("[kmain] IDT failed errno=%X\n", errno);
   if (ata_init() == 0) panic("[kmain] ATA failed errno=%X\n", errno);
 
   fid_t fid = vfs_open("/dev2/read.me", VFS_MODE_READ | VFS_MODE_WRITE);
   if (fid == -1) panic("[kmain] VFS_OPEN failed\n");
-  
-  /*
-  char msg[13] = {'\0'};
-  size_t bytes = vfs_read(fid, msg, sizeof(msg));
-  printf("[kmain] read %z with msg:%s.\n", bytes, msg);
-
-  if (strcmp(msg, "Hello there!")) strcpy(msg, "Ahoy there!");
-  else strcpy(msg, "Hello there!");
-  bytes = vfs_write(fid, msg, sizeof(msg));
-  printf("[kmain] wrote %z with msg:%s.\n", bytes, msg);
-
-  bytes = vfs_read(fid, msg, sizeof(msg));
-  printf("[kmain] read %z with msg:%s.\n", bytes, msg);
   */
-
+  
   printf("[kmain] REACHED END\n");
+  printf("[you] ");
+  char c;
+  while (true) {
+    serial_gets(COM1, &c, 1);
+    if (c < 'a' || c > '~')
+      printf("^C\n[you] ");
+    else
+      printf("%c", c);
+  }
   while (true);
 }
